898f33edd17942a89bd089ae1a433c3f2ddbdae9
Davi Gualberto <davigualbertofunecriacho@gmail.com>
2026-09-17 15:11:27 -0300

    Subindo os bglh

--- dispatcher.php
+++
@@ -1,8 +0,0 @@
-<?php 
-
-function dispatcher($rota){
-    echo "5. Dispatcher decidiu qual controller deve executar.<br>";
-    if ($rota === "/usuários") {
-        usuarioController();
-    }
-}
--- router.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-function router(){
-    echo "2. Router está analisando a URL.<br>";
-    $rota = "/usuarios";
-    $parametro = "id=123";
-    middleware($rota);
-}
